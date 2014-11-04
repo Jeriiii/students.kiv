420acf1be1ebdf50fe450d5a4949c647ccafc018
Jeriiii <p.kukral@nejlevnejsiwebstranky.cz>
2014-11-04 14:11:49 +0100

    přidání tiny mce

---
+++ app/views/admin/changeLink.php
@@ -0,0 +1,6 @@
+<h1>Změna odkazu</h1>
+<?php
+$form = "changeLinkForm&id=$linkId";
+include 'forms/linkForm.php';
+?>
+
---
+++ app/views/admin/forms/fileForm.php
@@ -0,0 +1,14 @@
+<form enctype="multipart/form-data" role="form" action="<?php echo $basePath . "/admin/files" ?>?do=submit-form&form=<?php echo $form; ?>" method="post">
+	<div class="form-group">
+		<label>Název souboru</label>
+		<input name="name" type="text" class="form-control" placeholder="Název odkazu" required="" value="<?php if (isset($fileName)) echo $fileName; ?>">
+	</div>
+	<?php if (empty($hideFileInput)) { ?>
+		<div class="form-group">
+			<label>Soubor</label>
+			<input type="file" name="fileToUpload" class="form-control" required="">
+		</div>
+	<?php } ?>
+	<input type="hidden" name="pageId" value="<?php echo $pageId ?>">
+	<input name="send" type="submit" value="Odeslat" class="btn btn-primary" />
+</form>
